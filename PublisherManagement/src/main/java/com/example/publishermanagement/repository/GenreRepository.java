81c49838c139b81b0f88db30205d6157f9c0666d
liwren <huangli1223@gmail.com>
2022-02-02 22:28:59 -0600

    updated GenreRepository

--- PublisherManagement/src/main/java/com/example/publishermanagement/repository/GenreRepository.java
+++ PublisherManagement/src/main/java/com/example/publishermanagement/repository/GenreRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface GenreRepository extends JpaRepository<Genre,Long> {
+    
     Genre findByName(String genre_name);
-    //  Genre findByName(String genre_name);
+
 
 }
